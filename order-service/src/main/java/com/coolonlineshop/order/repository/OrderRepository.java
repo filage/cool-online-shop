97ac3904c9612e429ad1dd7342258aa82d222748
filage <dranetren@mail.ru>
2026-06-22 11:12:41 +0300

    Add order repository

---
+++ order-service/src/main/java/com/coolonlineshop/order/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.coolonlineshop.order.repository;
+
+import com.coolonlineshop.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserIdOrderByCreatedAtDesc(Long userId);
+}
